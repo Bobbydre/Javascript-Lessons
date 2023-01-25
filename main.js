299926b3f6273bfc379acdc67f9993087f71fe46
Bobbydre <35635351+Bobbydre@users.noreply.github.com>
2023-01-25 11:02:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log("Hello World!")
+console.log("Hi there")
+console.log(typeof "Dare")
+console.log(typeof "42")
+console.log(typeof 42)
+console.log(typeof true)
+
+//STRINGS and STRING METHODS
+const myVariable = "Bobbydre"
+
+console.log(myVariable.charAt(4))
+console.log(myVariable.length)
+console.log(myVariable.indexOf("dre"))
+console.log(myVariable.lastIndexOf("by"))
+console.log(myVariable.slice(0, 5))
+console.log(myVariable.toUpperCase(""))
+console.log(myVariable.split(""))
